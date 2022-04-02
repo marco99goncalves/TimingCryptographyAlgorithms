55bebc7e9cf8308c121decec905f5a4f1839ae30
marco99goncalves <marco99goncalves@gmail.com>
2022-04-02 19:58:50 +0100

    Finished?

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import random
 import time
 import string
 from binascii import hexlify
-from cryptography.hazmat.primitives.ciphers import Cipher , algorithms , modes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -12,44 +12,74 @@ from statistics import mean
 from math import sqrt
 
 
-
 NUMBER_OF_FILES = 10
 
 
 def AES(numBytes):
-    timeList = []
+    encTime = []
+    decTime = []
     for i in range(NUMBER_OF_FILES):
         message = bytes(GenerateContent(numBytes), 'utf-8')
 
-        start = time.time()
-
-        #Encrypt the content
-        key = urandom (32)
+        # Encrypt the content
+        key = urandom(32)
         iv = urandom(16)
 
-        cipher = Cipher(algorithms.AES(key),modes.CTR(iv))
-        encryptor=cipher.encryptor()
+        start = time.time()
+        cipher = Cipher(algorithms.AES(key), modes.CTR(iv))
+        encryptor = cipher.encryptor()
 
-        ct = encryptor.update (message) + encryptor.finalize()
+        ct = encryptor.update(message) + encryptor.finalize()
 
         end = time.time()
-        timeList.append(end-start)
-    
-    return timeList
+        encTime.append(end-start)
+
+        # Decrypt
+        start = time.time()
+
+        # Codigo de desencriptação
+        plaintext = Cipher(algorithms.AES(key), modes.CTR(iv))
+        decryptor = cipher.encryptor()
+
+        pt = decryptor.update(ct) + decryptor.finalize()
+
+        end = time.time()
+        decTime.append(end-start)
+
+    return (encTime, decTime)
+
+
+def SHA256(numBytes):
+    encTime = []
+    decTime = []
+    for i in range(NUMBER_OF_FILES):
+        message = bytes(GenerateContent(numBytes), 'utf-8')
+        start = time.time()
+
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(message)
+        digest.finalize()
+
+        end = time.time()
+        encTime.append(end-start)
+
+    return (encTime, decTime)
+
 
 def RSA(numBytes):
-    timeList = []
+    encTime = []
+    decTime = []
     for i in range(NUMBER_OF_FILES):
-        #Encrypt the content
+        # Encrypt the content
         message = bytes(GenerateContent(numBytes), 'utf-8')
-        
-        start = time.time()
+
         private_key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048,
         )
         public_key = private_key.public_key()
 
+        start = time.time()
         ciphertext = public_key.encrypt(
             message,
             padding.OAEP(
@@ -60,30 +90,42 @@ def RSA(numBytes):
         )
 
         end = time.time()
-        timeList.append(end-start)
+        encTime.append(end-start)
 
-    return timeList
-
-
-def SHA256(numBytes):
-    timeList = []    
-    for i in range(NUMBER_OF_FILES):
-        message = GenerateContent(numBytes)
+        # Decrypt
         start = time.time()
 
-        digest = hashes.Hash(hashes.SHA256())
-        digest.update(bytes(message, 'utf-8'))
-        digest.finalize()
+        plaintext = private_key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
 
         end = time.time()
-        timeList.append(end-start)
-    
-    return timeList
-    
-def PrintConfidenceLevel(numBytes, timeList):
-    sd = std(timeList)
+        decTime.append(end-start)
+
+    return (encTime, decTime)
+
+
+def PrintConfidenceLevel(numBytes, encTime, decTime):
+    # Intervalo de confiança para encriptação
+    sd = std(encTime)
     conf_level = 1.96 * (sd/sqrt(NUMBER_OF_FILES))
-    print("Intervalo de confiança a 95% para " + str(numBytes) + "\n" + str(mean(timeList)) + " +- " + str(conf_level))
+    print("-------------- " + str(numBytes) + " bytes --------------")
+    print("Intervalo de confiança a 95% para tempo de encriptação: \n\t" +
+          str(mean(encTime)) + " +- " + str(conf_level))
+
+    if(len(decTime) > 0):
+        # Intervalo de confiança para desencriptação
+        sd = std(decTime)
+        conf_level = 1.96 * (sd/sqrt(NUMBER_OF_FILES))
+        print("\nIntervalo de confiança a 95% para tempo de desencriptação: \n\t" +
+              str(mean(decTime)) + " +- " + str(conf_level))
+        print("----------------------------------------\n")
+
 
 def GenerateContent(numBytes):
     content = ""
@@ -91,6 +133,8 @@ def GenerateContent(numBytes):
         content += str(random.choice(string.ascii_letters))
     return content
 
-l = [8, 64, 512, 4096, 32768, 262144, 2047152]
+
+l = [128, 128]
 for i in l:
-   PrintConfidenceLevel(i, AES(i))
+    encTime, decTime = RSA(i)
+    PrintConfidenceLevel(i, encTime, decTime)
